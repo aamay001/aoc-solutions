1f30bac7c621ad10005323cb357c5e17f30fcee2
Andy Amaya <contact@andyamaya.com>
2024-12-01 18:29:56 -0800

    Clear day 2 solution.

--- src/day-2/solution.tsx
+++ src/day-2/solution.tsx
@@ -1,12 +1,12 @@
-const day2Part1Solution = (data: string): number => {
-  throw new Error('not implemented',)
-}
+// const day2Part1Solution = (data: string): number => {
+//   throw new Error('not implemented',)
+// }
 
-const day2Part2Solution = (data: string): number => {
-  throw new Error('not implemented')
-}
+// const day2Part2Solution = (data: string): number => {
+//   throw new Error('not implemented')
+// }
 
-export {
-  day2Part1Solution,
-  day2Part2Solution,
-};
+// export {
+//   day2Part1Solution,
+//   day2Part2Solution,
+// };
