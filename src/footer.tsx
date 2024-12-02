3649e5d3411a60679cbd788904230034a139b300
Andy Amaya <contact@andyamaya.com>
2024-12-01 20:33:37 -0800

    Footer update.

--- src/footer.tsx
+++ src/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => (
     <br />
     <i style={{ fontSize: 'small' }}>
       I don't encourage you to use this to solve your puzzle!<br/>
-      Advent of Code is an opportunity to make something cool!<br/>
+      AoC++ is an opportunity to make something cool and level up your coding skills!<br/>
       Everything was coded without AI!
     </i>
     <p>Made by <a href="https://andyamaya.com" target="_blank">Andy Amaya</a></p>
