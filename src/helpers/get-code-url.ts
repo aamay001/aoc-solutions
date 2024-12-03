5dd47c56640018951f0d6adcaaa7674c1d2bf82d
Andy Amaya <contact@andyamaya.com>
2024-12-03 07:31:40 -0800

    Renaming solution files to .ts

--- src/helpers/get-code-url.ts
+++ src/helpers/get-code-url.ts
@@ -1,1 +1,1 @@
-export const getCodeUrl = (day: number): string => `https://raw.githubusercontent.com/aamay001/aoc-solutions/refs/heads/main/src/day-${day}/solution.tsx`;
+export const getCodeUrl = (day: number): string => `https://raw.githubusercontent.com/aamay001/aoc-solutions/refs/heads/main/src/day-${day}/solution.ts`;
