63166812677282021880d95397a6d2c79d0eadb0
Andy Amaya <contact@andyamaya.com>
2024-12-03 15:53:07 -0800

    Redirect for previous URL path scheme for day routes.

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,6 +1,17 @@
+import { useLocation, useNavigate } from "react-router";
 import RouteLayout from "./route-layout"
 
 const NotFound = () => {
+  const location = useLocation()
+  const navigate = useNavigate();
+
+  if (location.pathname.includes('day-')) {
+    const path = location.pathname;
+    const day = path.substring(path.length - 1);
+    navigate(`/day/${day}`)
+    return null;
+  }
+
   return (
     <RouteLayout name="404 - Not Found">
       <p>This solution is not available OR the URL you entered is invalid!</p>
