408a0f746f505b56e42753acd9e1dcd47632392d
Andy Amaya <contact@andyamaya.com>
2025-02-06 05:40:10 -0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,6 @@
 const Footer = () => (
   <footer>
     <br />
-    <i style={{ fontSize: 'small' }}>
-      Don't use this to solve your puzzle!<br/>
-      Use AoC++ to level up your coding skills!<br/>
-      The layout, navigation, and structure was coded without AI.<br />
-      Some solutions were coded or optimized with the help of AI.
-    </i>
     <p>Made by <a href="https://andyamaya.com" target="_blank">Andy Amaya</a></p>
     <p>
       <a href="https://github.com/aamay001/aoc-solutions" target="_blank">
